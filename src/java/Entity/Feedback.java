d8c4aca7415da428f45f6846bd700a2a343cd9bb
KhanhLyHihi <khanhly24002@gmail.com>
2024-04-22 21:47:12 +0700

    Shop1

---
+++ src/java/Entity/Feedback.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entity;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author DELL
+ */
+public class Feedback {
+    private int id;
+    private User user;
+    private Product product;
+    private String content;
+    private LocalDateTime date;
+
+    public Feedback() {
+    }
+
+    public Feedback(int id, User user, Product product, String content, LocalDateTime date) {
+        this.id = id;
+        this.user = user;
+        this.product = product;
+        this.content = content;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+    
+    
+}
